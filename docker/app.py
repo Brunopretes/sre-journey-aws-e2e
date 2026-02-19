669fb34ed77555d3bcd1aabdd5d0e6e3bdec0574
bruno <brunoppretes@gmail.com>
2026-02-19 18:53:09 -0300

    resolvendo conflito de nome de container e iniciando o debug interativo

--- docker/app.py
+++ docker/app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #simila uma falha aleatória de 20% para testarmos a resiliência depois
-    if random.random() < 0.2: 
-	return "Erro interno do servidor", 500
-    return "Olá, SRE! aplicação rodando com sucesso no docker"
+    # Simula uma falha aleatória de 20% para testarmos resiliência depois
+    if random.random() < 0.2:
+        return "Erro Interno do Servidor", 500
+    return "Olá SRE! Aplicação rodando com sucesso no Docker."
 
 @app.route('/health')
 def health():
     return "OK", 200
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-
